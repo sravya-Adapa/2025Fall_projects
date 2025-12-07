79479461dc8d9b483616834fe31ae5740fe3de3d
Sravya Adapa <sravyaadapa@Sravyas-MacBook-Air-2.local>
2025-12-06 20:13:53 -0600

    Simulationv1

--- Simulation.py
+++ Simulation.py
@@ -1,127 +1,118 @@
 import pandas as pd
 import networkx as nx
 import numpy as np
-import matplotlib.pyplot as plt
+import pickle
 import os
 
-NUM_SIMULATIONS= 10000 # number of simulations
-PROBABILITY_OF_FAILURE = 0.05 # the probability baseline for a node to fail.
-#test value
-total_cogs = 100000
-total_units = 100
+# CONFIGURATION
+NUM_SIMS = 10000
+P_FAIL_BASE = 0.05
+TOTAL_COGS_REFERENCE = 80_240_000_000
 
-def build_network():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    industry_path = os.path.join(base_dir, "Tesla_specific_data", "BEA_industry_to_commodity_updated_data.csv")
-    supplier_path = os.path.join(base_dir, "Tesla_specific_data", "Supplier_to_BEA_commodity_mapping.csv")
 
-    # Load the data
-    df_industries = pd.read_csv(industry_path)
-    df_suppliers = pd.read_csv(supplier_path)
-
-    # Layer 0/ root node
-    G = nx.DiGraph()
-    root_node = "Tesla"
-    G.add_node(root_node, layer=0)
-
-    # Layer1(industries)
-    for _, row in df_industries.iterrows():
-        industry = row["Industry Description"]
-        weight = row["Motor vehicles, bodies and trailers, and parts"]
-        G.add_edge(industry, root_node, weight=weight, layer=1)
-
-    # Layer2(suppliers)
-    supplier_col = df_suppliers.columns[0]
-    industry_col = df_suppliers.columns[1]
-    grouped = df_suppliers.groupby(industry_col)[supplier_col].apply(list)
-    for industry, suppliers in grouped.items():
-        if G.has_node(industry):
-            # randomized weight dependencies for suppliers to each industry
-            n = len(suppliers)
-            splits = np.random.rand(n)
-            splits = splits / np.sum(splits)
-            for supplier, split in zip(suppliers, splits):
-                G.add_edge(supplier, industry, weight=split, layer=2)
+def load_graph_from_pickle(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Graph file not found at: {file_path}")
+    with open(file_path, 'rb') as f:
+        G = pickle.load(f)
     return G
 
-def simulate(G,n_simulations=NUM_SIMULATIONS,p_failure=PROBABILITY_OF_FAILURE):
-    # to store results for each simulation
-    unit_results = []
-    trapped_value_results = []
-    suppliers = [n for n, d in G.in_degree() if d == 0] # suppliers lists from the graph if there is no incoming edge then it is considered as supplier
-    industries = [n for n in G.predecessors("Tesla")] # industry list from graph
-    for _ in range(n_simulations):
-        # assign random failure probability to each supplier.
-        # If the random failure probability is less than PROBABILITY_OF_FAILURE only then that supplier node will fail.
-        supplier_fail_probability = np.random.rand(len(suppliers))
-        fail = supplier_fail_probability < p_failure
-        # list of failed suppliers
-        failed_suppliers = [
-            supplier for supplier, is_failed in zip(suppliers, fail)
-            if is_failed
-        ]
 
-        # Calculate how much percent of supply each industry have after failing random supplier nodes.
-        # starting with 100% supply if one fails subtract the weight of the edge from the total supply.
-        # after subtracting all weights of failed nodes we get the final supply for each industry.
-        industry_supply = {}
+def load_industry_cogs():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(base_dir, "Tesla_specific_data", "preprocessed_data", "component_weights_2024_percent.csv")
+    df = pd.read_csv(csv_path)
+    df.columns = df.columns.str.strip()
+    cogs_industry = df.set_index('component_industry_description')['cogs_allocated_usd'].to_dict()
+
+    return cogs_industry
+
+
+def simulation(G, cogs_map):
+
+    # Find
+    suppliers = [n for n, d in G.in_degree() if d == 0]
+    if G.has_node("Tesla"):
+        root = "Tesla"
+    industries = [n for n in G.predecessors(root)]
+
+    # Get Original COGS
+    original_industry_cogs = {}
+    for ind in industries:
+        original_industry_cogs[ind] = cogs_map[ind]
+
+    # Results
+    simulated_total_cogs_results = []
+    bottleneck_counts = {ind: 0 for ind in industries}
+
+    for _ in range(NUM_SIMS):
+
+        # to determine which supplier nodes to fail
+        is_failed = np.random.rand(len(suppliers)) < P_FAIL_BASE
+        failed_indices = np.where(is_failed)[0]
+
+        # to determine the severity of failure. Starting with 1 for every industry and changing it bases of failure severity
+        supplier_damage = np.zeros(len(suppliers))
+        if len(failed_indices) > 0:
+            failure_severity = np.random.uniform(0.3, 1.0, size=len(failed_indices))
+            supplier_damage[failed_indices] = failure_severity
+        supplier_dict = dict(zip(suppliers, supplier_damage))
+
+        # step 2 calculate remaining COGS %
+        total_COGS_available = 0.0
+        remaining_industry_percentage = {}
+
         for industry in industries:
-            current_supply = 1
-            for supplier,_,data in G.in_edges(industry, data=True):
-                if supplier in failed_suppliers:
-                    current_supply -= data["weight"]
-            industry_supply[industry] = max(0, current_supply)
+            percentage_left = 1.0
+            for supplier, _, data in G.in_edges(industry, data=True):
+                weight = data.get('weight', 0)
+                supplier_damage = supplier_dict.get(supplier, 0)
+                loss = weight * supplier_damage
+                percentage_left -= loss
+            percentage_left = max(0.0, percentage_left)
+            remaining_industry_percentage[industry] = percentage_left
 
-        # check the weakest industry. If batteries(industry) are left with 40% of original supply and all other industries have more than 40%
-        # then Tesla’s production is limited to 40%, since overall output cannot exceed the weakest link in the supply chain.
-        min_supply = min(industry_supply.values())
+            # Value Left = Original Dollar Amount * % Available
+            value_left = original_industry_cogs[industry] * percentage_left
+            total_COGS_available += value_left
+        simulated_total_cogs_results.append(total_COGS_available)
 
-        # 1. Production loss interms of units
-        loss_percentage = 1- min_supply
-        units_loss = loss_percentage * total_units
-        unit_results.append(loss_percentage)
+        # step -3 Find bottleneck industry
+        min_percentage = min(remaining_industry_percentage.values())
+        if min_percentage < 1.0:
+            bottleneck = [k for k, v in remaining_industry_percentage.items() if v == min_percentage]
+            for b in bottleneck:
+                bottleneck_counts[b] += 1
 
-        # 2. Trapped Value
-        trapped_value = 0.0
+    return simulated_total_cogs_results, bottleneck_counts
 
-        for industry, supply_level in industry_supply.items():
-            excess_portion = supply_level - min_supply
-            industry_weight = G[industry]["Tesla"]['weight']
-            #Value = Excess% * Weight_of_Industry * Total_Annual_COGS
-            value_stuck = excess_portion * industry_weight * total_cogs
-            trapped_value += value_stuck
-
-        trapped_value_results.append(trapped_value)
-    return unit_results, trapped_value_results
 
 if __name__ == "__main__":
-    G = build_network()
-    unit_results, trapped_value_results = simulate(G)
-    avg_units = np.mean(unit_results)
-    p95_units = np.percentile(unit_results, 95)
 
-    avg_trapped = np.mean(trapped_value_results)
-    p95_trapped = np.percentile(trapped_value_results, 95)
+    # Load paths
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    graph_path = os.path.join(base_dir, "supply_chain_graph", "tesla_supply_chain_graph_2024.pkl")
 
-    print(f"PRODUCTION LOSS (UNITS)")
-    print(f"Average: {avg_units:,.0f} vehicles")
-    print(f"P95 Risk: {p95_units:,.0f} vehicles")
+    # Load network graph
+    G = load_graph_from_pickle(graph_path)
 
-    print(f"TRAPPED INVENTORY VALUE ($)")
-    print(f"Average: ${avg_trapped}")
-    print(f"P95 Risk: ${p95_trapped}")
+    # Load cogs per industry
+    industry_cogs = load_industry_cogs()
 
+    # Run simulation
+    simulation_cogs, bottlenecks = simulation(G, industry_cogs)
+    avg_cogs_sum = np.mean(simulation_cogs)
+    reduction_sum = TOTAL_COGS_REFERENCE - avg_cogs_sum
+    shortfalls = [TOTAL_COGS_REFERENCE - x for x in simulation_cogs]
+    p95_shortfall = np.percentile(shortfalls, 95)
 
-
-
-
-
-
-
-
-
-
-
-
-
+    # Print Results
+    print(f"Total Baseline COGS:        ${TOTAL_COGS_REFERENCE / 1e9:.2f}B")
+    print(f"Simulated Avg Availability: ${avg_cogs_sum / 1e9:.2f}B")
+    print(f"Average Shortfall:          ${reduction_sum / 1e9:.2f}B ({(reduction_sum / TOTAL_COGS_REFERENCE):.1%})")
+    print(f"P95 Shortfall (Risk):       ${p95_shortfall / 1e9:.2f}B")
+    sorted_bn = sorted(bottlenecks.items(), key=lambda x: x[1], reverse=True)
+    for ind, count in sorted_bn[:5]:
+        freq = (count / NUM_SIMS) * 100
+        print(f"{ind}{' ' * (40 - len(str(ind)))} | {freq:.1f}% of runs")
 
